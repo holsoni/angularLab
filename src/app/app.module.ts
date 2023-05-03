13f3df66f02f1e5c36a9b631e1aea48eb6606085
holsoni <soniakostashchuk@gmail,com>
2023-05-03 15:17:31 +0300

    lab3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,37 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LeftNavBarComponent } from './left-nav-bar/left-nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { FilmsComponent } from './films/films.component';
+import { ClipsComponent } from './clips/clips.component';
+import { SeriesComponent } from './series/series.component';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterLink, RouterModule} from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
+import { InfoComponent } from './info/info.component';
+import { CustomPipePipe } from './custom-pipe.pipe';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LeftNavBarComponent,
-    FooterComponent
+    FooterComponent,
+    MainComponent,
+    FilmsComponent,
+    ClipsComponent,
+    SeriesComponent,
+    InfoComponent,
+    CustomPipePipe,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
